688472147aa660f4ac3a985461f816c7fa49bb28
Márk Kővári <kovarimarkofficial@gmail.com>
2022-12-02 06:55:44 +0100

    fix: add second day solution

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,7 @@ fn main() {
     println!("1st day =>");
     println!("\t {}", get_1_first());
     println!("\t {}", get_1_second());
+    println!("2nd day =>");
+    println!("\t {}", get_2_first());
+    println!("\t {}", get_2_second());
 }
