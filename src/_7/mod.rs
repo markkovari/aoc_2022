37fe0660f017168b50673d480229abd9bd0798d0
Márk Kővári <kovarimarkofficial@gmail.com>
2022-12-07 10:39:18 +0100

    chore: add init for 7

---
+++ src/_7/mod.rs
@@ -0,0 +1,18 @@
+pub fn get_7_first() -> i32 {
+    let content = include_str!("../../inputs/7/input.data");
+    2
+}
+pub fn get_7_second() -> i32 {
+    let content = include_str!("../../inputs/7/input.data");
+    2
+}
+
+#[cfg(test)]
+mod test_7 {
+    use super::*;
+
+    #[test]
+    fn test_examples_a_lot() {
+        assert_eq!(1, 1);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-use _1::{get_1_first, get_1_second};
-use _2::{get_2_first, get_2_second};
-use _3::{get_3_first, get_3_second};
-use _4::{get_4_first, get_4_second};
-use _5::{get_5_first, get_5_second};
-use _6::{get_6_first, get_6_second};
-
 mod _1;
 mod _2;
 mod _3;
 mod _4;
 mod _5;
 mod _6;
+mod _7;
+
+use _1::{get_1_first, get_1_second};
+use _2::{get_2_first, get_2_second};
+use _3::{get_3_first, get_3_second};
+use _4::{get_4_first, get_4_second};
+use _5::{get_5_first, get_5_second};
+use _6::{get_6_first, get_6_second};
+use _7::{get_7_first, get_7_second};
+
 
 fn main() {
     println!("1st day =>");
@@ -31,4 +34,7 @@ fn main() {
     println!("6th day =>");
     println!("\t {}", get_6_first());
     println!("\t {}", get_6_second());
+    println!("7th day =>");
+    println!("\t {}", get_7_first());
+    println!("\t {}", get_7_second());
 }
